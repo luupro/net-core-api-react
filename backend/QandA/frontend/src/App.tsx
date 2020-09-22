c0fdc5439f6c1d047377b9405d76aa61784d03ba
Do Ngoc Luu <dnluu@tctav.com>
2020-09-22 17:14:07 +0700

    lazy loading routes

--- backend/QandA/frontend/src/App.tsx
+++ backend/QandA/frontend/src/App.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
 
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
+import { QuestionPage } from './QuestionPage';
+const AskPage = lazy(() => import('./AskPage'));
+
 const App: React.FC = () => {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Switch>
+          <Redirect from="/home" to="/" />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/ask" component={AskPage} />
+          <Route path="/signin" component={SignInPage} />
+          <Route path="/questions/:questionId" component={QuestionPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 

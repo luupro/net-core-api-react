91430a0af22a745e9764a2d4fd1c65a198c40626
Do Ngoc Luu <dnluu@tctav.com>
2020-10-28 15:32:31 +0700

    update source code

---
+++ backend/QandA/Controllers/QuestionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QandA.Data;
+using QandA.Data.Models;
+using System.Collections.Generic;
+
+namespace QandA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public QuestionsController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            var questions = _dataRepository.GetQuestions();
+            return questions;
+        }
+    }
+}
